1116c365c3a1b14e65b13518eb765a9c9287d829
nbrustein <nbrustein@gmail.com>
2013-07-15 00:11:25 -0400

    a comment

--- lib/ferret/adapters.rb
+++ lib/ferret/adapters.rb
@@ -2,6 +2,10 @@ require File.expand_path("../../ferret", __FILE__)
 
 module Ferret::Adapters
   
+  # note. adapters have to handle time inside of hashes.  So, if we want to
+  # just drop a json blob into an hbase, cell, for example, we have to user
+  # bson instead of json
+  
   def self.get(name)
     {
       'mongodb' => Ferret::Adapters::Mongo
